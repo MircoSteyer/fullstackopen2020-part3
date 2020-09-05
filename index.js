7e907991f0346c6a8389ab7d966fb4de706cd47d
unknown <mircosteyer@web.de>
2020-09-05 11:10:20 +0200

    buildui

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 let persons = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
